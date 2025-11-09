04e835bd7baddb8e0004f0f4c7e1516130825157
Kartel <criticalcartel@gmail.com>
2025-11-09 18:14:14 +0000

    Added some methods

--- pages/02b_understanding-methods/script.js
+++ pages/02b_understanding-methods/script.js
@@ -1,11 +1,45 @@
 import gsap from 'gsap';
-
-gsap.to('.box', {
+const animation = gsap.to('.box', {
   background: '#9b5782ff',
   borderRadius: '50%',
   rotation: 360,
   duration: 1.5,
   scale: 1.1,
-  repeat: 3,
-  yoyo: true,
+  // repeat: 4,
+  // yoyo: true,
+});
+
+const play = document.querySelector('.play');
+const pause = document.querySelector('.pause');
+const resume = document.querySelector('.resume');
+const restart = document.querySelector('.restart');
+const reverse = document.querySelector('.reverse');
+const kill = document.querySelector('.kill');
+const yoyo = document.querySelector('.yoyo');
+const repeat = document.querySelector('.repeat');
+repeat;
+
+play.addEventListener('click', () => {
+  animation.play();
+});
+pause.addEventListener('click', () => {
+  animation.pause();
+});
+resume.addEventListener('click', () => {
+  animation.resume();
+});
+restart.addEventListener('click', () => {
+  animation.restart();
+});
+reverse.addEventListener('click', () => {
+  animation.reverse();
+});
+kill.addEventListener('click', () => {
+  animation.kill();
+});
+yoyo.addEventListener('click', () => {
+  animation.yoyo(true);
+});
+repeat.addEventListener('click', () => {
+  animation.repeat(2);
 });
